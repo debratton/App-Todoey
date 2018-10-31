91d588a9c1598e146d7ae5c719832f99d0e43040
debratton <debratton@gmail.com>
2018-10-31 11:23:32 -0500

    Done for Day

--- Todoey/Todoey/Controller/MainTVC.swift
+++ Todoey/Todoey/Controller/MainTVC.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class MainTVC: UITableViewController {
 
-    var itemsArray = ["David Bratton", "Anna Bratton", "Nicholas Bratton", "Alexandra Bratton", "Lucy Bratton", "Marley Bratton", "Bailey Bratton"]
+//    var itemsArray = ["David Bratton", "Anna Bratton", "Nicholas Bratton", "Alexandra Bratton", "Lucy Bratton", "Marley Bratton", "Bailey Bratton"]
+    //var itemsArray:[String] = []
+    var itemsArray = [ToDo]()
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+//        let itemObject = UserDefaults.standard.object(forKey: "ToDoListArray")
+//        if let tempItems = itemObject as? [ToDo] {
+//            itemsArray = tempItems
+//        }
+        let newToDo1 = ToDo()
+        newToDo1.title = "David Bratton"
+        itemsArray.append(newToDo1)
+        
+        let newToDo2 = ToDo()
+        newToDo2.title = "Anna Bratton"
+        itemsArray.append(newToDo2)
+        
+        let newToDo3 = ToDo()
+        newToDo3.title = "Nicholas Bratton"
+        itemsArray.append(newToDo3)
+        
+        let newToDo4 = ToDo()
+        newToDo4.title = "Alexandra Bratton"
+        itemsArray.append(newToDo4)
+        
+        let newToDo5 = ToDo()
+        newToDo5.title = "Robert Bratton"
+        itemsArray.append(newToDo5)
+        
+        
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -24,8 +51,16 @@ class MainTVC: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCell", for: indexPath)
-
-        cell.textLabel?.text = itemsArray[indexPath.row]
+        // HAVE TO CHANGE THIS ONCE YOU USE CLASS MODEL
+        //cell.textLabel?.text = itemsArray[indexPath.row]
+        cell.textLabel?.text = itemsArray[indexPath.row].title
+        
+        if itemsArray[indexPath.row].done == true {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+            
+        }
 
         return cell
     }
@@ -36,11 +71,14 @@ class MainTVC: UITableViewController {
         // PRINT ARRAY VALUE
         print(itemsArray[indexPath.row])
         // IF STATEMENT TO SET CHECKMARK ON SELECTED ROW
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        
+        if itemsArray[indexPath.row].done == false {
+            itemsArray[indexPath.row].done = true
         } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+            itemsArray[indexPath.row].done = false
         }
+        // FORCES CHECKMARKS TO UPDATE
+        tableView.reloadData()
         // MAKE ROW GO BACK TO WHITE AFTER CLICKED INSTEAD OF STAYING GREY
         tableView.deselectRow(at: indexPath, animated: true
             
@@ -54,9 +92,11 @@ class MainTVC: UITableViewController {
         let alert = UIAlertController(title: "Add New ToDoey", message: "", preferredStyle: .alert)
         // ADD TITLE AND THIS IS WHERE YOU HAVE TO ADD PRINT AND APPEND TO ARRAY
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            let newToDo = ToDo()
             if let newItem = textField.text {
-                print("Added: \(newItem)")
-                self.itemsArray.append(newItem)
+                newToDo.title = newItem
+                self.itemsArray.append(newToDo)
+                self.defaults.set(self.itemsArray, forKey: "ToDoListArray")
                 self.tableView.reloadData()
             }
         }
---
+++ Todoey/Todoey/Model/ToDo.swift
@@ -0,0 +1,15 @@
+//
+//  ToDo.swift
+//  Todoey
+//
+//  Created by David E Bratton on 10/30/18.
+//  Copyright © 2018 David Bratton. All rights reserved.
+//
+
+import Foundation
+
+class ToDo {
+    var title: String = ""
+    var done: Bool = false
+    
+}
